546d07412be576ecabba23855809b647a9b64fbd
naiyaarampat <127419961+naiyaarampat@users.noreply.github.com>
2023-03-21 11:32:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ def encode(password):  # function that changes the password by adding 3
     return final
 
 
-# pass1 = input()
-# print(encode(pass1))
-
-
 def menu():  # prints out the menu for the main fucntion
     print("Menu")
     print("-------------")
@@ -21,21 +17,19 @@ def menu():  # prints out the menu for the main fucntion
     print("2. Decode")
     print("3. Quit")
 
+    
+def decode(password):  # function that decodes the password
+    updated_pass = []  # list to store the final password
+    for i in password:  # iterates through each part of string
+        i = int(i)  # changes each to an int
+        i -= 3
+        i = str(i)  # back to string so it can go in list
+        updated_pass.append(i)  # adds to list
+    final = ''.join(updated_pass)
+    return final
 
-# def decode(password):  # function that decodes the password
-#     updated_pass = []  # list to store the final password
-#     for i in password:  # iterates through each part of string
-#         i = int(i)  # changes each to an int
-#         i -= 3
-#         i = str(i)  # back to string so it can go in list
-#         updated_pass.append(i)  # adds to list
-#     final = ''.join(updated_pass)
-#     return final
-#
-#
-# pass2 = input()
-# print(decode(pass2))
 
+stored_pass = ""
 if __name__ == "__main__":
     while True:  # while loop for menu so user van keep entering option
         menu()  # prints menu each time
@@ -45,7 +39,7 @@ if __name__ == "__main__":
             stored_pass = encode(password1)
             print("Your password has been encoded and stored!")  # does not print encoded password, just stores it
         elif inp == 2:  # function from partner
-            pass
+            print(f"The encoded password is {stored_pass}, and the original password is {decode(stored_pass)}.")
         elif inp == 3:  # quits the entire
             break
 
